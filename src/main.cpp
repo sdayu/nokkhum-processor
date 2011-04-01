05310d9f99b0aacc8d40c649f48b366172e86601
Thanathip Limna <boatkrap@gmail.com>
2011-04-01 18:23:27 +0700

    add main source and hello Nok Khum message

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Thanathip Limna
+// Version     :
+// Copyright   :
+// Description : Hello World in C++
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World, Hello Nuk Khum" << endl; // prints Hello World, Hello Nukkhum
+	return 0;
+}
