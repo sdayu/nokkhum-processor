037dcfea1b0653a8c2e7c44ee94854df2a8d2a9a
Thanathip Limna <boatkrap@gmail.com>
2011-06-01 11:03:36 +0700

    add new Mat queue that return cv::Mat when call pop() method and clone the cv::Mat before return

---
+++ src/util/cv_mat_queue.hpp
@@ -0,0 +1,31 @@
+/*
+ * cv_mat_queue.hpp
+ *
+ *  Created on: Jun 1, 2011
+ *      Author: boatkrap
+ */
+
+#ifndef NOKKHUM_CV_MAT_QUEUE_HPP_
+#define NOKKHUM_CV_MAT_QUEUE_HPP_
+
+#include <queue>
+#include <opencv/cv.h>
+
+namespace nokkhum {
+
+class CvMatQueue {
+public:
+	CvMatQueue();
+	virtual ~CvMatQueue();
+
+	cv::Mat pop();
+	void push(cv::Mat);
+	bool empty();
+	int size();
+private:
+	std::queue<cv::Mat> cv_mat_queue;
+};
+
+}
+
+#endif /* NOKKHUM_CV_MAT_QUEUE_HPP_ */
